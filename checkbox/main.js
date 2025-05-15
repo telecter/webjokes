b35ecf5f62a107acfa7b6ea6a5bc58bb6384265d
telecter <alexcidon9@gmail.com>
2025-05-14 21:13:21 -0400

    add checkbox game

---
+++ checkbox/main.js
@@ -0,0 +1,23 @@
+const container = document.getElementById("checkbox-container");
+
+function createCheckboxes() {
+  for (const child of container.children) {
+    if (Math.random() > 0.9) {
+      child.checked = false;
+    }
+  }
+  let r = Math.floor(Math.random() * (15 - 2 + 1)) + 2;
+  for (let i = 0; i < r; i++) {
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    if (i == r-1) {
+      checkbox.addEventListener("click", (e) => {
+        createCheckboxes();
+        checkbox.removeEventListener("click", this)
+      }, { once: true });
+    }
+    container.appendChild(checkbox);
+  }
+}
+
+createCheckboxes();
